d47b257640fc620a7dd077663534b9970f3f095e
Sonu Jha <159139463+Sonujha2808@users.noreply.github.com>
2024-12-26 02:02:55 +0530

    Update homepage.js

--- Registration/SignUP-SignIn-Form-with-connection-to-FIrebase/homepage.js
+++ Registration/SignUP-SignIn-Form-with-connection-to-FIrebase/homepage.js
@@ -3,13 +3,7 @@ import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/fi
 import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAOVJH5tV8QLm0hIjRq7UI9ZyefjS1ktxY",
-    authDomain: "task-3-c3f07.firebaseapp.com",
-    projectId: "task-3-c3f07",
-    storageBucket: "task-3-c3f07.firebasestorage.app",
-    messagingSenderId: "273872126924",
-    appId: "1:273872126924:web:089e91edfa1d64b4ccd369",
-    measurementId: "G-VPFSGHC51Z"
+    //add firebaseConfig from website 
   };
 
 const app = initializeApp(firebaseConfig);
